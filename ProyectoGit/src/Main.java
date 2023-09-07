282a0b48e1e52a5674c159ecb2fd372005ade3eb
SergioEncinaRamos <sergioencinaramos@gmail.com>
2023-09-07 07:30:42 +0200

    Git amend demo (#1)
    * Isn´t the first commit
    
    * Added method to say bye!
    
    * Added comment
    
    * Añadido un segundo comentario
    
    * Añado un 3 comentario
    
    * Por aquí viene el cuarto comentario
    
    ---------
    
    Co-authored-by: Sergio Encina Ramos <ESX0004240_prosegur>

---
+++ ProyectoGit/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        sayBye();
+    }
+
+    public static void sayBye() {
+        System.out.println("Good bye");
+    }
+
+    //Esto es un comentario
+
+    //Añado un nuevo comentario para squeashear
+
+    //Añado el 3 comentario
+
+    //Añado el cuarto comentario
+}
